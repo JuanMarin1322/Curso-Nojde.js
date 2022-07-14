157ef6042efeb8facde4a635f5956045b1b3d204
JuanMarin1322 <91574074+JuanMarin1322@users.noreply.github.com>
2022-07-14 12:20:35 -0500

    Session 6
    Peticiones HTTP con Api de clima, trabajada en consola en donde podemos buscar una ciudad del mundo para conocer su temperatura actual

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -18,25 +18,39 @@ const main = async() => {
         switch (opt) {
 
           case 1:
-            const termino = await leerInput('Ciudad: ');
+            // Mostrar mensaje
+           const termino = await leerInput('Ciudad: ');
+             // Buscar los lugares
            const lugares= await busquedas.ciudad( termino );
+           // Seleccionar el lugar
            const id = await listadoLugares(lugares);
 
+           if (id === '0' ) continue;
            const lugarSeleccionado = lugares.find(l => l.id === id);
-      
+           
+           busquedas.agregarHistorial(lugarSeleccionado.nombre);
+           
+           const climaCiudad =await busquedas.climaLugar(lugarSeleccionado.lat,lugarSeleccionado.lng);
+            
             console.log('\nInformacion de la ciudad\n'.green)
             console.log('Ciudad:', lugarSeleccionado.nombre)
             console.log('Lat:', lugarSeleccionado.lat)
             console.log('Lng:', lugarSeleccionado.lng)
-            console.log('Temperatura:',)
-            console.log('Minima:',)
-            console.log('Maxima:',)
-            break
+            console.log('Temperatura:', ` ${climaCiudad.temp + ' °C'} `.yellow)
+            console.log('Minima:', ` ${climaCiudad.min + ' °C'} `.yellow)
+            console.log('Maxima:', ` ${climaCiudad.max + ' °C'} `.yellow)
+            console.log('Como está el clima:', climaCiudad.desc.green)
+            
+            break;
             
           case 2:
-            break;
 
-          case 3:
+            busquedas.historialCapitalizado.forEach( (lugar,i) => {
+              const idx = `${ i +1}`.green;
+
+              console.log(`${idx} ${ lugar}`)
+
+          })
             break;
 
         }
--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -1,13 +1,29 @@
+const fs = require('fs');
+
+
 const axios = require('axios');
+
 
 class Busquedas {
 
 
     historial = [];
+    dbPath = './db/database.json';
 
     constructor () {
-        
+        this.leerDB();
 
+    }
+
+    get historialCapitalizado() {
+
+        return this.historial.map( lugar => {
+
+            let palabras = lugar.split(' ');
+            palabras = palabras.map(p => p[0].toUpperCase() + p.substring(1));
+
+            return palabras.join(' ')
+        })
     }
 
     get paramasMapBox () {
@@ -23,12 +39,12 @@ class Busquedas {
 
         try {
 
-            const intance = axios.create({
+            const instance = axios.create({
                 baseURL:`https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
                 params : this.paramasMapBox
             });
 
-            const resp = await intance.get();
+            const resp = await instance.get();
             // const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Palmira.json?proximity=ip&types=place%2Cpostcode%2Caddress&language=es&access_token=pk.eyJ1IjoianVhbm1hcm5uIiwiYSI6ImNsNWtqZm8wMzBhbzMzamw2dGRtZ2t2cXAifQ.Elxjzws6ycUUSjI7ydrCJg')
             // console.log('Ciudad', lugar);
             return resp.data.features.map( lugar => ({
@@ -37,14 +53,98 @@ class Busquedas {
                     nombre : lugar.place_name,
                     lng: lugar.center[0],
                     lat: lugar.center[1]
-            }))
+            }));
             
         } catch (error) {
-            
+            return []
         }
+
+    }
+    get paramWeather () {
+
+       return {
+            'appid' : process.env.OPENWEATHER_KEY,
+            'units' : 'metric',
+            'lang': 'es'
+    }
     }
 
-}
+    async climaLugar( lat, lon) {
+
+        try {
+            
+
+                const instance =axios.create({
+                    baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+                    params: { 
+                        ...this.paramWeather, lat,lon
+                            
+                    }
+
+                });
+
+                const resp = await instance.get();
+                const { weather, main} = resp.data
+                
+                return{
+                    desc: weather[0].description,
+                    min : main.temp_min,
+                    max : main.temp_max,
+                    temp: main.temp
+            };
+
+        } catch (error) {
+            console.log(error);
+        }
+
+
+            }
+
+
+            agregarHistorial( lugar =' '){
+
+
+                if(this.historial.includes( lugar.toLocaleLowerCase() )){
+
+                    return;
+                }
+
+                this.historial = this.historial.splice(0,10);
+
+                this.historial.unshift( lugar.toLocaleLowerCase() );
+
+                this.guaradrDB();
+
+            }
+
+            guaradrDB(){
+
+                const payload = {
+
+                    historial: this.historial
+                };
+
+                 fs.writeFileSync( this.dbPath, JSON.stringify( payload ) );
+
+            }
+
+            leerDB(){
+
+
+                if( !fs.existsSync( this.dbPath)){
+
+                    return null;
+            
+                }
+            
+                const info = fs.readFileSync (this.dbPath, { encoding: 'utf-8'});
+                const data = JSON.parse(info);
+            
+                this.historial = data.historial; 
+            }
+
+
+        }
 
 
 module.exports = Busquedas;
